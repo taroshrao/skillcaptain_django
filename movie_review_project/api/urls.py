536cb791350bc8f4634bab2d2f5cf259f43cf176
taroshrao <taroshrao@gmail.com>
2025-04-24 14:15:34 +0530

    commit

--- movie_review_project/api/urls.py
+++ movie_review_project/api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path 
 from .views import welcome 
 from django.urls import path
-from .views import welcome, movie_list, review_list_create
+from .views import welcome, movie_list, review_list_create, register
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('welcome/', welcome),
     path('movies/', movie_list),
     path('movies/<int:movie_id>/reviews/', review_list_create),
+    path('register/', register),
+    path('login/', obtain_auth_token), # DRF's built-in login view
 ]
--- movie_review_project/api/views.py
+++ movie_review_project/api/views.py
@@ -4,11 +4,16 @@ from django.shortcuts import render
 from .models import Review
 from .serializers import ReviewSerializer
 from django.http import JsonResponse 
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Movie
 from .serializers import MovieSerializer
- 
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+
 def welcome(request): 
     return JsonResponse({
         "message": "Welcome to the Movie Review API!"
@@ -23,6 +28,7 @@ def movie_list(request):
     return Response(serializer.data)
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def review_list_create(request, movie_id):
     if request.method == 'GET':
         reviews = Review.objects.filter(movie_id=movie_id)
@@ -37,3 +43,22 @@ def review_list_create(request, movie_id):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+@api_view(['POST'])
+def register(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    if not username or not password:
+        return Response({"error": "Username and password required."}, status=400)
+
+    if User.objects.filter(username=username).exists():
+        return Response({"error": "Username already exists."}, status=400)
+
+    user = User.objects.create_user(username=username, password=password)
+    token = Token.objects.create(user=user)
+
+    return Response({
+        "message": "User registered successfully",
+        "token": token.key
+    }, status=201)
